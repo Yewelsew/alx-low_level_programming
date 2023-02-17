e41b53d09da1ad587969220e226c20bde1f08c3c
root <mess>
2023-02-17 13:14:02 -0800

    print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,7 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+/* Auth yewlsew*/
 
 /**
  * main - Prints all possible combinations of single-digit numbers.
@@ -7,19 +10,19 @@
  */
 int main(void)
 {
-	int num;
+        int num;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
-	}
+        for (num = 0; num <= 9; num++)
+        {
+                putchar((num % 10) + '0');
+                if (num == 9)
+                        continue;
 
-	putchar('\n');
+                putchar(',');
+                putchar(' ');
+        }
 
-	return (0);
+        putchar('\n');
+
+        return (0);
 }
