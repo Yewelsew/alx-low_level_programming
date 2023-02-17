aafddd10cdbf3a178115ec3dd3288914446a1558
root <mess>
2023-02-17 14:49:08 -0800

    t_effect

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdio.h>
 
 /**
  * main - Prints all possible combinations of single-digit numbers.
@@ -10,17 +8,17 @@
 int main(void)
 {
 	int num;
-	
+
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-
+		
 		putchar(',');
 		putchar(' ');
 	}
-	
+
 	putchar('\n');
 
 	return (0);
