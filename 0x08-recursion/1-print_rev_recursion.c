258a8ac79da1282c4eb6eb2b6f2903ec080cbb9e
root <mess>
2023-03-08 02:25:49 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
- * @s: string
  * _print_rev_recursion - Prints a string in reverse
+ * @s: string
  *
- * Return: Always return nithing.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void _print_rev_recursion(char *s)
 {
