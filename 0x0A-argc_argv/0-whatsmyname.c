40e6bb99e0981781a2cb5570158f62081f788a7f
root <mess>
2023-03-10 03:33:13 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - to prints the name of the file
+ * @argc: is an argument counter for char
+ * @argv: is an argument value for char
+ * Return: null
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
