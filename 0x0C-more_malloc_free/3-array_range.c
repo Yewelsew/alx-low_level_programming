0786914d0b4d57125b8a282918d3bcc39f95524d
root <mess>
2023-03-16 05:38:55 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,31 +1,31 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- * @min: incoming parameter
- * @max: incoming parameter
- * Return: Nothing.
+ * array_range - function that creates an array of integers.
+ * @min: input
+ * @max: input
+ * Return: output
  */
 int *array_range(int min, int max)
 {
-	int *ptr;
-	int idx, len;
+	int *ar;
+	int i = 0;
 
 	if (min > max)
+	{
 		return (NULL);
-
-	len = max - min + 1;
-	ptr = malloc(len * sizeof(int));
-	if (!ptr)
+	}
+	ar = malloc(sizeof(int) * (max - min + 1));
+	if (ar == NULL)
+	{
 		return (NULL);
-
-	for (idx = 0; idx < len; idx++)
-		ptr[idx] = min++;
-
-	return (ptr);
+	}
+	while (min <= max)
+	{
+		ar[i] = min;
+		i++;
+		min++;
+	}
+	return (ar);
 }
