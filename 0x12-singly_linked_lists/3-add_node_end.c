9028a597d2383dc227892a922756137b6adc331b
root <Henok>
2023-03-30 02:44:20 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,69 +2,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
-/**
-  * _strlen - gets length of the string
-  * @s: string
-  * Return: length of the string
-  */
-int _strlen(const char *s)
-{
-	int i;
 
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
-/**
-  * _strdup - recreation of string duplicate function
-  * @src: source of string to duplicate
-  * Return: pointer to malloc'd space with copied string
-  */
-void *_strdup(const char *src)
-{
-	int len, i;
-	char *dest;
 
-	len = _strlen(src);
-	dest = malloc((len + 1) * sizeof(char));
-	if (dest == NULL)
-		return (NULL);
-	for (i = 0; src[i]; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
-}
 /**
-  * add_node_end - add new nodes to the end of the list
-  * @head: current place in the list
-  * @str: string to add to the head
-  * Return: pointer to current position in list
-  */
+ * add_node_end - adds a node at the end of list
+ * @head: points to the head of list
+ * @str: what goes in the new node
+ * Return: address of new node or NULL
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *current;
-	char *dupstr;
+	list_t *newTail;
+	list_t *ptr = *head;
 
-	if (str == NULL)
+	if (!head || !str)
 		return (NULL);
-	dupstr = _strdup(str);
-	if (dupstr == NULL)
-		return (NULL);
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = dupstr;
-	new->len = _strlen(str);
-	new->next = NULL;
 
-	if (*head == NULL)
+	newTail = malloc(sizeof(list_t));
+	if (!newTail)
+		return (NULL);
+
+	newTail->str = strdup(str);
+	if (!newTail->str)
 	{
-		*head = new;
-		return (*head);
+		free(newTail);
+		return (NULL);
 	}
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new;
-	return (*head);
+
+	newTail->len = _strlen(newTail->str);
+	newTail->next = NULL;
+
+	if (ptr)
+	{
+		while (ptr->next)
+			ptr = ptr->next;
+		ptr->next = newTail;
+	}
+	else
+		*head = newTail;
+	return (newTail);
 }
